f6e578ad7a79b4af7cdf20e66ea8ddc3de2186b2
wan <wandiwen@qq.com>
2015-07-12 21:49:05 +0800

    添加条件缓存相关功能

--- 简易Web服务器/TCPconnect.h
+++ 简易Web服务器/TCPconnect.h
@@ -2,6 +2,7 @@
 #include "stdafx.h"
 using std::string;
 enum Method{GET, POST, HEAD, UNKNOWN};
+#define SERVER "Server: wan's web server\r\n"
 class TCPconnect
 {
 public:
@@ -10,8 +11,11 @@ public:
 	static DWORD WINAPI connecting(LPVOID t_client_socket);
 
 private:
-	static const int request_buffer_size = 1 << 10;//1k
-	static const int reponse_buffer_size = 1 << 10;//1KB
+	//buffer_size 不超过64KB
+	static const int buffer_size_1K = 1 << 10;//1KB
+	static const int buffer_size_32K = 1 << 15;//32KB
+	static const int default_buffer_size = buffer_size_32K;
+	static const int send_limit = 1 << 20;//设置1M为阈值，超过1M将使用206
 
 public:
 	TCPconnect();
@@ -20,22 +24,49 @@ public:
 	//解析报文
 	void analyze();
 	// 发送响应报文
-	bool reponse();
+	bool response();
 private:
 	// 打开URL所申请的资源文件
 	bool openFile();
 	// 获得所请求资源的MIME类型
 	string getFileType();
+	// 获得当前服务器时间
+	string getDate();
+	//分析请求头部
+	void analyze_request_header(string field_name, string field_value);
+	// 请求头部的参数设为默认值
+	void set_default();
+	void response_OK();
+	void response_Error(string error_no, string error_descript, string error_msg);
+	void response_Part_Content();//返回是否传输完所有文件
+	void response_Not_Modified();
+	void response_Not_Implemented();
 private:
 	SOCKET client_socket;//该TCP连接的客户端socket
+//----------------  request  -----------------//
 	char *request_buffer;//请求报文的buffer
-	char *reponse_buffer;//响应报文的buffer
-	string status_line;//状态行
-	string reponse_header;//响应头域
-	HANDLE hThread;//线程handle
-	
+	int request_buffer_size;//请求报文buffer大小
 	Method method;//请求的方法
-	string abs_path;//请求的据对路径
+	string url;//请求的据对路径
+	bool range;
+	int range_min;
+	int range_max;
+	bool Connect_keep_alive;//是否保持持续连接
+	//条件GET
+	bool if_get;
+	string if_none_match;
+	string if_modified_since;
+	//POST方法
+	int content_length;
+//---------------- reponse  ----------------//
+	char *response_buffer;//响应报文的buffer
+	int response_buffer_size;//响应报文buffer大小
+	string status_line;//状态行
+	string response_header;//响应头域
 	FILE *file;
+	int file_length;
+	struct stat file_status;//文件的详细信息
+	string last_modified;//文件的最后修改时间
+	string Etag;//实体标签
 };
 
